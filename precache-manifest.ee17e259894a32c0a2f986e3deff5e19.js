7090af5c17aa2264d5dab3df8629520984d602fd
Thomas Champion <thomas.champion@orchestra.eu>
2020-05-15 22:08:07 +0200

    Updates

---
+++ precache-manifest.ee17e259894a32c0a2f986e3deff5e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5720832dce46b06b88fa0338823337",
+    "url": "/sailing/index.html"
+  },
+  {
+    "revision": "09e931a68d41740b0a41",
+    "url": "/sailing/static/css/main.9e9d2192.chunk.css"
+  },
+  {
+    "revision": "392fdc0ddb462ec12b72",
+    "url": "/sailing/static/js/2.9c9fdc0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sailing/static/js/2.9c9fdc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e931a68d41740b0a41",
+    "url": "/sailing/static/js/main.142c7305.chunk.js"
+  },
+  {
+    "revision": "8440634055a47ecfbab3",
+    "url": "/sailing/static/js/runtime-main.9a62e6c7.js"
+  }
+]);
